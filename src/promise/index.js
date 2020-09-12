7b3312047d43a374c40dcf34d4c7858c5e078e5e
johan suarez <jhoanseb1992@gmail.com>
2020-09-12 21:04:34 +1000

    class 3 - API request with promise

--- src/promise/index.js
+++ src/promise/index.js
@@ -11,3 +11,24 @@ const somethingWillHappen = () => {
 somethingWillHappen()
   .then(response => console.log(response))
   .catch(error => console.log(error))
+
+const somethingWillHappen2 = () => {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      setTimeout(() => {
+        resolve('True')
+      }, 2000)
+    } else {
+      const error = new Error('Ups!!');
+      reject(error);
+    }
+  })
+}
+
+somethingWillHappen2()
+  .then(response => console.log(response))
+  .catch(error => console.error(error))
+
+Promise.all([somethingWillHappen(), somethingWillHappen2()])
+  .then(response => console.log('array = ', response))
+  .catch(error => console.error(error))
